8853300e16bc5e2c4cd40955f80a7a40e7329379
Harshada <harshadabaravkar9999@gmail.com>
2024-01-14 12:33:24 +0530

    Test case initialised

--- src/test/java/TestLayer/LoginPageTest.java
+++ src/test/java/TestLayer/LoginPageTest.java
@@ -1,5 +1,13 @@
 package TestLayer;
 
+import org.testng.annotations.Test;
+
 public class LoginPageTest {
+	
+	@Test
+	public static void testcas()
+	{
+		System.out.println("Abc test case");
+	}
 
 }
